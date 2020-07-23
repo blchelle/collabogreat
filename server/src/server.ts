58bad52380ad1d44a083d9b0bf9efb74eea3314a
Brock Chelle <brocklchelle@gmail.com>
2020-07-23 08:52:23 -0600

    setup some basic routes for creating and getting projects

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,34 @@
-import dotenv from 'dotenv';
+/* eslint-disable no-console */
 import path from 'path';
-import server from './app';
+import dotenv from 'dotenv';
+import App from './app';
+import ProjectController from './controllers/projectController';
 
+process.on('uncaughtException', (error) => {
+	console.error('UNCAUGHT EXCEPTION');
+	console.log(error.name, error.message);
+	console.log(error.stack);
+
+	process.exit(1);
+});
+
+// Read the configuration file
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-// Initialize the server
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-	console.log(`Server running on port ${port}...`);
-});
+// Verify the port number
+const port = Number(process.env.PORT) || 8000;
+const app = new App([ProjectController.getInstance()], port);
+
+// Pull and verify MongoDB credentials
+if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
+	throw new Error('Database access credentials are not supplies');
+}
+
+// Connect to MongoDB
+const database = process.env.DATABASE;
+const databasePassword = process.env.DATABASE_PASSWORD;
+const db = database.replace('<PASSWORD>', databasePassword);
+App.connectToMongoDB(db);
+
+// Start the Server
+app.listen();
