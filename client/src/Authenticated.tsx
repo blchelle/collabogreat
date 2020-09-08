d3fef485ab151fba88b039ab33a240f2b0950c6f
Brock Chelle <brocklchelle@gmail.com>
2020-09-08 08:41:38 -0600

    Created the NotFound Page
    Modified the routing such that the CGAppBar doesn't appear on the NotFound page
    Modified the container for the dasboard

--- client/src/Authenticated.tsx
+++ client/src/Authenticated.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
 
+import Routes from './routes';
 import CGAppBar from './components/CGAppBar/CGAppBar.component';
 import CreateProjectDialog from './components/CreateProjectDialog/CreateProjectDialog.component';
 import Loading from './pages/Loading/Loading.page';
@@ -10,18 +11,25 @@ const NotFound = React.lazy(() => import('./pages/NotFound/NotFound.page'));
 const ProjectHub = React.lazy(() => import('./pages/ProjectHub/ProjectHub.page'));
 
 const Authenticated = () => {
+	const { pathname } = useLocation();
+
 	return (
 		<Suspense fallback={Loading}>
-			<CGAppBar />
-			<Switch>
-				<Route exact path='/'>
-					<Redirect to='/dashboard' />
-				</Route>
-				<Route exact path='/dashboard' component={Dashboard} />
-				<Route exact path='/loading' component={Loading} />
-				<Route path='/projects/:id' component={ProjectHub} />
-				<Route path='*' component={NotFound} />
-			</Switch>
+			{Object.values(Routes).includes(pathname) ? (
+				<>
+					<CGAppBar />
+					<Switch>
+						<Route exact path='/'>
+							<Redirect to='/dashboard' />
+						</Route>
+						<Route exact path='/dashboard' component={Dashboard} />
+						<Route exact path='/loading' component={Loading} />
+						<Route path='/projects/:id' component={ProjectHub} />
+					</Switch>
+				</>
+			) : (
+				<NotFound />
+			)}
 			<CreateProjectDialog />
 		</Suspense>
 	);
