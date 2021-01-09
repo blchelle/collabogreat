1190ba91cc76d6726185f71a4eeef0d41d75a8e4
Brock Chelle <Brocklchelle@gmail.com>
2021-01-09 10:29:20 -0700

    Implemented notifications for project invitations

--- client/src/components/NotificationsDropdown/NotificationsDropdown.component.tsx
+++ client/src/components/NotificationsDropdown/NotificationsDropdown.component.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Button, Grid, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { Divider, Grid, List, ListItem, Typography } from '@material-ui/core';
 
+import { RootState } from '../../redux/root.reducer';
+import NotificationItem from '../NotificationItem/NotificationItem.component';
 import { ReactComponent as NoNotifications } from '../../assets/no-notifications.svg';
 
 const NotificationsDropdown: React.FC = () => {
-	return (
-		<Grid container direction='column' alignItems='center' spacing={2}>
-			<Typography variant='h6' gutterBottom>
-				You&apos;re all caught up!
-			</Typography>
-			<NoNotifications style={{ width: '40%' }} />
-			<Typography variant='body1' align='center'>
-				We don&apos;t have anything to notify you about yet,
-				<br />
-				but we&apos;ll put any notifications right here
-			</Typography>
-			<Grid item>
-				<Button color='primary'>Edit Notification Settings</Button>
+	const projectInvitations = useSelector((state: RootState) => state.user!.projectInvitations);
+
+	if (projectInvitations.length === 0) {
+		return (
+			<Grid container direction='column' alignItems='center' spacing={2}>
+				<Typography variant='h6' gutterBottom>
+					You&apos;re all caught up!
+				</Typography>
+				<NoNotifications style={{ width: '40%' }} />
+				<Typography variant='body1' align='center'>
+					We don&apos;t have anything to notify you about yet,
+					<br />
+					but we&apos;ll put any notifications right here
+				</Typography>
 			</Grid>
-		</Grid>
+		);
+	}
+
+	return (
+		<List>
+			{projectInvitations.map((project) => (
+				<>
+					<ListItem>
+						<NotificationItem _id={project._id!} type='project' title={project.title!} />
+					</ListItem>
+					<Divider />
+				</>
+			))}
+		</List>
 	);
 };
 
