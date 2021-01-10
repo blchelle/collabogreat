173fa5f4ac88ace6eb93ad374a9feba9aa47e036
Brock Chelle <Brocklchelle@gmail.com>
2021-01-10 12:29:03 -0700

    Modified Dashboard to match the issue spec

--- client/src/components/TasksContainer/TasksContainer.component.tsx
+++ client/src/components/TasksContainer/TasksContainer.component.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Divider, Grid, Typography } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { Button, Divider, Grid, Typography } from '@material-ui/core';
+
 import TaskCard from '../TaskCard/TaskCard.component';
+import { ReactComponent as UDNoTasks } from '../../assets/no-tasks.undraw.svg';
+import { openModal } from '../../redux/modals/modals.actions';
+import { ModalNames } from '../../redux/modals/modals.reducer';
 import { Task } from '../../redux/tasks/tasks.types';
+
+import useStyles from './TasksContainer.mui';
 
 interface TasksContainerProps {
 	type: 'user' | 'project';
@@ -9,6 +16,12 @@ interface TasksContainerProps {
 }
 
 const TasksContainer: React.FC<TasksContainerProps> = ({ type, tasks }) => {
+	// MUI
+	const classes = useStyles();
+
+	// Redux Hooks
+	const dispatch = useDispatch();
+
 	return (
 		<Grid container spacing={2}>
 			<Grid item xs>
@@ -17,13 +30,37 @@ const TasksContainer: React.FC<TasksContainerProps> = ({ type, tasks }) => {
 				</Typography>
 				<Divider />
 			</Grid>
-			<Grid item container spacing={3} alignItems='stretch'>
-				{tasks.map((task) => (
-					<Grid item xs={12} sm={6} lg={4} key={task._id}>
-						<TaskCard task={task} showAssignee={type === 'project'} />
+			{tasks.length === 0 ? (
+				<Grid item container direction='column' justify='center' alignItems='center' spacing={2}>
+					<UDNoTasks className={classes.noTasksSVG} />
+					<Grid item>
+						<Typography variant='h6'>
+							{type === 'user'
+								? "You haven't created any tasks yet, click the button below to create your first task"
+								: 'This project has no active tasks, click the button below to add one'}
+						</Typography>
 					</Grid>
-				))}
-			</Grid>
+					<Grid item>
+						<Button
+							variant='contained'
+							color='primary'
+							onClick={() =>
+								dispatch(openModal(ModalNames.CREATE_TASK_DIALOG, { open: true, children: null }))
+							}
+						>
+							{type === 'user' ? 'Create Your First Task' : 'Create a Task'}
+						</Button>
+					</Grid>
+				</Grid>
+			) : (
+				<Grid item container spacing={3} alignItems='stretch'>
+					{tasks.map((task) => (
+						<Grid item xs={12} sm={6} lg={4} key={task._id}>
+							<TaskCard task={task} showAssignee={type === 'project'} />
+						</Grid>
+					))}
+				</Grid>
+			)}
 		</Grid>
 	);
 };
--- client/src/pages/Dashboard/Dashboard.page.tsx
+++ client/src/pages/Dashboard/Dashboard.page.tsx
@@ -12,6 +12,7 @@ const Dashboard: React.FC = () => {
 
 	const userId = useSelector((state: RootState) => state.user!._id);
 	const tasks = useSelector((state: RootState) => state.tasks.filter((t) => t.user === userId));
+	const projects = useSelector((state: RootState) => state.projects);
 
 	return (
 		<>
@@ -19,13 +20,15 @@ const Dashboard: React.FC = () => {
 				<Typography color='textPrimary'>Dashboard</Typography>
 			</Breadcrumbs>
 			<Container maxWidth='xl'>
-				<Grid container direction='column' spacing={10}>
+				<Grid container direction='column' spacing={6}>
 					<Grid item>
-						<ProjectsContainer />
+						<ProjectsContainer projects={projects} />
 					</Grid>
-					<Grid item>
-						<TasksContainer type='user' tasks={tasks} />
-					</Grid>
+					{projects.length > 0 ? (
+						<Grid item>
+							<TasksContainer type='user' tasks={tasks} />
+						</Grid>
+					) : null}
 				</Grid>
 			</Container>
 		</>
