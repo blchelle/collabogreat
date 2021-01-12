c6cdf943bdc570b56b2984aed646c51a842c6a5e
Brock Chelle <Brocklchelle@gmail.com>
2021-01-12 07:06:16 -0700

    Modified Notification UI

--- client/src/components/NotificationItem/NotificationItem.component.tsx
+++ client/src/components/NotificationItem/NotificationItem.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { Card, Grid, Typography } from '@material-ui/core';
 
 import ColoredAvatar from '../ColoredAvatar/ColoredAvatar.component';
 import LoadingButton from '../LoadingButton/LoadingButton.component';
@@ -26,80 +26,74 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ _id, type, title, c
 	const dispatch = useDispatch();
 
 	return (
-		<Card>
-			<CardContent>
-				<Grid container spacing={2} style={{ width: 400 }}>
-					{type === 'project' ? (
-						<>
-							<Grid item container alignItems='center' spacing={2}>
-								<Grid item>
-									<ColoredAvatar id={_id} text={title} />
-								</Grid>
-								<Grid item xs>
-									<Typography>{`You've been invited to join the project '${title}'`}</Typography>
-								</Grid>
-							</Grid>
-							<Grid item container spacing={2}>
-								<Grid item>
-									<LoadingButton
-										id={`notification accept ${_id}`}
-										color='primary'
-										variant='contained'
-										onClick={() => {
-											dispatch(
-												acceptInviteStart(user!.projects, user!.projectInvitations, _id, user!._id)
-											);
-										}}
-									>
-										Accept
-									</LoadingButton>
-								</Grid>
-								<Grid item>
-									<LoadingButton
-										id={`notification reject ${_id}`}
-										color='default'
-										onClick={() => {
-											dispatch(rejectInviteStart(_id, user!.projectInvitations));
-										}}
-									>
-										Reject
-									</LoadingButton>
-								</Grid>
-							</Grid>
-						</>
-					) : (
-						<>
-							<Grid item xs={12} container alignItems='center' spacing={2}>
-								<Grid item>
-									<Card
-										style={{
-											backgroundColor: color,
-											height: theme.spacing(5),
-											width: theme.spacing(5),
-										}}
-										elevation={0}
-									/>
-								</Grid>
-								<Grid item xs>
-									<Typography>{`You've been assigned to the task '${title}'`}</Typography>
-								</Grid>
-							</Grid>
-							<Grid item container spacing={2}>
-								<Grid item>
-									<LoadingButton
-										id={`notification dismiss ${_id}`}
-										onClick={() => dispatch(dismissTaskStart(_id, user!.newTasks))}
-										color='primary'
-									>
-										Dismiss
-									</LoadingButton>
-								</Grid>
-							</Grid>
-						</>
-					)}
-				</Grid>
-			</CardContent>
-		</Card>
+		<Grid container direction='column' spacing={1} style={{ width: 400 }}>
+			{type === 'project' ? (
+				<>
+					<Grid item xs={12} container alignItems='center' spacing={2}>
+						<Grid item>
+							<ColoredAvatar id={_id} text={title} />
+						</Grid>
+						<Grid item xs>
+							<Typography>{`You've been invited to join the project '${title}'`}</Typography>
+						</Grid>
+					</Grid>
+					<Grid item container spacing={1}>
+						<Grid item>
+							<LoadingButton
+								id={`notification accept ${_id}`}
+								color='primary'
+								variant='contained'
+								onClick={() => {
+									dispatch(
+										acceptInviteStart(user!.projects, user!.projectInvitations, _id, user!._id)
+									);
+								}}
+							>
+								Accept
+							</LoadingButton>
+						</Grid>
+						<Grid item>
+							<LoadingButton
+								id={`notification reject ${_id}`}
+								color='default'
+								onClick={() => {
+									dispatch(rejectInviteStart(_id, user!.projectInvitations));
+								}}
+							>
+								Reject
+							</LoadingButton>
+						</Grid>
+					</Grid>
+				</>
+			) : (
+				<>
+					<Grid item xs={12} container alignItems='center' spacing={2}>
+						<Grid item>
+							<Card
+								style={{
+									backgroundColor: color,
+									height: theme.spacing(5),
+									width: theme.spacing(5),
+								}}
+								elevation={0}
+							/>
+						</Grid>
+						<Grid item xs>
+							<Typography>{`You've been assigned to the task '${title}'`}</Typography>
+						</Grid>
+					</Grid>
+					<Grid item>
+						<LoadingButton
+							id={`notification dismiss ${_id}`}
+							onClick={() => dispatch(dismissTaskStart(_id, user!.newTasks))}
+							color='primary'
+						>
+							Dismiss
+						</LoadingButton>
+					</Grid>
+				</>
+			)}
+		</Grid>
 	);
 };
 
