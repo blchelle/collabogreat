1ea31aca5035046a94bda8a83cada3cea64d6843
Brock Chelle <Brocklchelle@gmail.com>
2021-01-18 15:22:10 -0700

    Added data to generate demo environments from

---
+++ api/src/data/projects.data.ts
@@ -0,0 +1,16 @@
+const projects = [
+	{
+		title: 'Keep It',
+		description:
+			'Keep it is an mobile iOS app that helps users keep track of all their important belongings',
+		board: ['Requested', 'To Do', 'In Progress', 'Done'],
+	},
+	{
+		title: 'Boromi',
+		description:
+			'Boromi is an Android app build with Java that empowers people to read more by providing an easy to use book exchange system',
+		board: ['To Do', 'In Progress', 'Completed'],
+	},
+];
+
+export default projects;
